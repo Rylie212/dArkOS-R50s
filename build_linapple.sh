0506801544195681007ba8fea7fb3e46927e3802
Christian Haitian <christianhaitian0@gmail.com>
2025-06-30 17:01:19 -0400

    Fix OpenBor and linapple builds

--- build_linapple.sh
+++ build_linapple.sh
@@ -5,15 +5,14 @@ call_chroot "cd /home/ark &&
   cd ${CHIPSET}_core_builds &&
   chmod 777 builds-alt.sh &&
   eatmydata ./builds-alt.sh linapplesa &&
-  cd linapple/sdl2-compat/build &&
+  cd linapple/sdl12-compat/build &&
   make install
   "
 sudo mkdir -p Arkbuild/opt/linapple
-sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapple-64/linapple Arkbuild/opt/linapple/
+sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapplesa-64/linapple Arkbuild/opt/linapple/
 sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapple/res/Master.dsk Arkbuild/opt/linapple/
 sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapple/res/A2_BASIC.SYM Arkbuild/opt/linapple/
 sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapple/res/APPLE2E.SYM Arkbuild/opt/linapple/
-sudo cp -a Arkbuild/home/ark/${CHIPSET}_core_builds/linapple/LICENSE Arkbuild/opt/linapple/
 sudo cp -a linapple/gamecontrollerdb.txt Arkbuild/opt/linapple/
 sudo cp -R -a linapple/configs/* Arkbuild/opt/linapple/
 sudo cp linapple/apple2.sh Arkbuild/usr/local/bin/

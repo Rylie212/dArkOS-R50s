3be65b765ef85511208718b6c427576298c18a39
Christian Haitian <christianhaitian0@gmail.com>
2026-01-18 23:51:09 -0500

    Update gametank.sh to address potential audio issue for the standalone emulator

--- gametank/scripts/gametank.sh
+++ gametank/scripts/gametank.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 if [[ $1 == "standalone" ]]; then
+  export ALSA_CONFIG_PATH="/usr/share/alsa/alsa.conf.gametank"
+  cp -f /home/ark/.asoundrc /home/ark/.asoundrc.gametank
+  if [[ "$(tr -d '\0' < /proc/device-tree/compatible)" == *"rk3566"* ]]; then
+    sed -i '/ipc_key         1024/s//ipc_key         1024\n        ipc_key_add_uid 1/' /home/ark/.asoundrc.gametank
+  else
+    sed -i '/ipc_key 1024/s//ipc_key 1024\n ipc_key_add_uid 1/' /home/ark/.asoundrc.gametank
+  fi
+
   echo "VAR=GameTankEmulator" > /home/ark/.config/KILLIT
   sudo systemctl restart killer_daemon.service
 
@@ -8,7 +16,7 @@ if [[ $1 == "standalone" ]]; then
   export SDL_GAMECONTROLLERCONFIG_FILE="/opt/inttools/gamecontrollerdb.txt"
   /opt/inttools/gptokeyb -c "/opt/gametank/gametank.gptk" > /dev/null &
 
-  sudo /opt/gametank/GameTankEmulator "$2"
+  /opt/gametank/GameTankEmulator "$2"
 
   pgrep -f gptokeyb | sudo xargs kill -9
   unset SDL_GAMECONTROLLERCONFIG_FILE
